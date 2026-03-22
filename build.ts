f3b7d21a5676f6f12b1c6c8e5f01a0b1c640e14c
Evan Verworn <evan@verworn.ca>
2026-03-22 00:48:52 -0400

    public path /

--- build.ts
+++ build.ts
@@ -140,6 +140,7 @@ const result = await Bun.build({
   minify: true,
   target: "browser",
   sourcemap: "linked",
+  publicPath: "/",
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
